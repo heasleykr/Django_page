2763159449776d7cd4bb48f227b689ed8eeabb0f
heasleykr@gmail.com <Partytime1@!>
2020-11-29 11:39:34 -0800

    Initial commit for Django web application.

---
+++ pages/tests.py
@@ -0,0 +1,12 @@
+# from django.test import TestCase. Use this one for a project with DB
+from django.test import SimpleTestCase #used bc we AREN'T using a DB. 
+
+# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
